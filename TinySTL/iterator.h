9197a26ad6127e69a9c3ae2f1edfd361b336e9dc
zhangxule <zhangxule20020105@outlook.com>
2025-12-14 14:39:48 +0800

    更改目录结构

---
+++ TinySTL/iterator.h
@@ -0,0 +1,7 @@
+#ifndef TINYSTL_ITERATOR_H_
+#define TINYSTL_ITERATOR_H_
+
+// 这个头文件用于迭代器设计，包含了一些模板结构体与全局函数
+#include <cstddef> // ptrdiff_t, size_t
+#include "type_traits.h"
+#endif // !TINYSTL_ITERATOR_H_
---
+++ TinySTL/type_traits.h
@@ -0,0 +1,9 @@
+#idendef TINYSTL_TYPE_TRAITS_H_
+#define TINYSTL_TYPE_TRAITS_H_
+
+// 这个头文件用于提取类型信息
+
+//  use standard header for type traits
+#include <type_traits>
+
+#endif // !TINYSTL_TYPE_TRAITS_H_
